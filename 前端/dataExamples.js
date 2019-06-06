3dd46f5d26198aa45305ac202e510b8ec898161d
jslijin <42512810+jslijin@users.noreply.github.com>
2019-06-06 21:30:11 +0800

    Update dataExamples.js

--- 前端/dataExamples.js
+++ 前端/dataExamples.js
@@ -9,4 +9,20 @@ const e = {
 const data = [];
 for (let i=0; i<10; i++) data.push(e);
 
-const nam = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+// const nam = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const nam = [];
+
+nam.push("阿基米朵");
+nam.push("哈利波特");
+nam.push("小太阳");
+nam.push("非单色辐射");
+nam.push("黑熊");
+nam.push("吼吼");
+nam.push("谁主沉浮");
+nam.push("啦啦啦啦");
+nam.push("quailty");
+nam.push("白给");
+
+
+
