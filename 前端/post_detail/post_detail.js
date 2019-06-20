6bcac9a06b9b69cacf901a76241ce5db35128801
jslijin <jslijin2016@qq.com>
2019-06-20 17:05:26 +0800

    123

--- 前端/post_detail/post_detail.js
+++ 前端/post_detail/post_detail.js
@@ -1,23 +1,31 @@
 document.write("<script type='text/javascript' src='../usefulFunction.js'></script>");
 document.write("<script type='text/javascript' src='../dataExamples.js'></script>");
 
-let pagenum=1;
-let page_url="post_detail.html?id="+idnum+"&page=";
+var pagenum=1;
+let page_url;
 
 $(document).ready(function () {
-
+    page_url=window.location.href.toString();
 });
 
-$(document).load(function () {
-    pagenum=1;
-});
+function change(s,x) {
+    let t=s.lastIndexOf('=');
+    return s.substring(0,t+1)+x;
+}
+
+function getpage(s) {
+    let t=s.lastIndexOf('=');
+    return s.substring(t+1,t.length);
+}
 
 function go_next() {
+    pagenum=parseInt(getpage(page_url));
     pagenum++;
-    window.location.href=page_url+pagenum;
+    window.location.href=change(page_url, pagenum);
 }
 
 function go_prec() {
+    pagenum=parseInt(getpage(page_url));
     if (pagenum>1) pagenum--;
-    window.location.href=page_url+pagenum;
+    window.location.href=change(page_url, pagenum);
 }
