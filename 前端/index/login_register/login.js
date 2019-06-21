d82aeb9331c58871459cfa3bc72f667c547f3db5
jslijin <jslijin2016@qq.com>
2019-06-21 19:17:25 +0800

    232

--- 前端/index/login_register/login.js
+++ 前端/index/login_register/login.js
@@ -19,12 +19,15 @@ function try_to_login() {
     }
 
     globalPost("/login/login",{"name":name, "pwd": s},function (d) {
-        if (d["code"]===200) {
-            alert("登录成功!!!");
-        } else {
-            alert("登录失败!!!可能是用户名密码不匹配!!");
-            return;
-        }
+
+        console.log(d);
+
+        // if (d["code"]===200) {
+        //     alert("登录成功!!!");
+        // } else {
+        //     alert("登录失败!!!可能是用户名密码不匹配!!");
+        //     return;
+        // }
         setSession("user_name_login",name);
         setSession("user_id_login",d["data"]["userid"]);
         setSession("isLogin",1);
@@ -37,6 +40,6 @@ function try_to_login() {
             if (d["code"]===200 && d["data"]) setSession("isTeacher",1); else setSession("isTeacher",0);
         });
 
-        return_homepage();
+        // return_homepage();
     })
 }
