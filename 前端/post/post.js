11a16c1a62278cf67c397abae085c57d57be2863
jslijin <jslijin2016@qq.com>
2019-06-21 17:02:46 +0800

    1212

--- 前端/post/post.js
+++ 前端/post/post.js
@@ -2,7 +2,27 @@ document.write("<script type='text/javascript' src='../usefulFunction.js'></scri
 document.write("<script type='text/javascript' src='../dataExamples.js'></script>");
 document.write("<script type='text/javascript' src='../constAPI.js'></script>");
 
+function post() {
 
+    let content=$("#_content_id").text();
+    let title=$("#_title_id").text();
+    let type=getSession("type");
+    let userid=getSession("user_id_login");
+
+    globalPost("/posts/newPosts",{
+        "userid":userid,
+        "type":type,
+        "title":title,
+        "info":content,
+        "authority":0
+    },function (d) {
+        if (d["code"]===200) {
+            alert("发帖成功!!");
+            return;
+        } else alert("发帖失败!!");
+    })
+
+}
 
 
 
